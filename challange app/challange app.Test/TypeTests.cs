0e4f7e8d7df9d6fd1792d02b47f3bead6eed9a4c
Mateusz Świedrski <mateusz.s106@wp.pl>
2023-03-25 21:30:03 +0100

    zadanie domowe dziń8

---
+++ challange app/challange app.Test/TypeTests.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace challange_app.Test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void NameWitchWeHaveArentTheSame()
+        {
+            //arrange
+            string Student1 = "Kuba";
+            string Student2 = "Mateusz";
+            //act
+
+            //assert
+            Assert.AreNotEqual(Student1, Student2);
+        }
+        [Test]
+        public void TestNumberWitchWeHaveReturnTheSameResult()
+        {
+            //arrange
+            float number = 0324243234;
+           float number2 = 0324243234;
+            //act
+
+            //assert
+            Assert.AreEqual(number, number2);
+        }
+        [Test]
+        public void TestNumberWitchWeHaveReturnDifrentResult()
+        {
+            //arrange
+            int number = 0;
+            int number2 = 2;
+            //act
+
+            //assert
+            Assert.AreNotEqual(number, number2);
+        }
+            [Test]
+        public void GetEmployeeShouldRetunDifrentObjects()
+        {
+            //arrange
+            var Employee1 = GetEmployee("Mateusz", "Kowalski", "22");
+            var Employee2 = GetEmployee("piotr", "Kowalski", "22");
+            //act
+
+            //assert
+            Assert.AreNotEqual(Employee1, Employee2);
+        }
+        private Employee GetEmployee(string name, string surname, string age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
